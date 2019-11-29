ce1d4e0639300f68adcf674397814bab4db937c7
Steve T <steve@flossie.com>
2019-11-29 02:04:38 +0000

    Add support for EnableAddBillCard parameter when making purchase request

--- src/Message/PxPostPurchaseRequest.php
+++ src/Message/PxPostPurchaseRequest.php
@@ -8,4 +8,41 @@ namespace Omnipay\PaymentExpress\Message;
 class PxPostPurchaseRequest extends PxPostAuthorizeRequest
 {
     protected $action = 'Purchase';
+
+    /** 
+     * Get the PxPost EnableAddBillCard
+     *
+     * Optional boolean value to enable the creation of a repeat billing token
+     * when purchasing.
+     *
+     * @return mixed
+     */
+    public function getEnableAddBillCard()
+    {   
+        if (is_null($this->getParameter('enableAddBillCard')))
+          return false;
+
+        return $this->getParameter('enableAddBillCard');
+    }   
+
+    /** 
+     * Set the PxPost EnableAddBillCard
+     *
+     * @param bool $value
+     * @return $this
+     */
+    public function setEnableAddBillCard($value)
+    {   
+        return $this->setParameter('enableAddBillCard', $value);
+    }   
+
+    public function getData()
+    {
+        $data = parent::getData();
+
+        $data->EnableAddBillCard = ($this->getEnableAddBillCard()) ? 1 : 0;
+
+        return $data;
+    }
+
 }
