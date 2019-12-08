e02190e56854f883966f1d4b117df5de4b9ca76f
Steve T <steve@flossie.com>
2019-12-08 01:41:11 +0000

    Moved flossie specific code into authorize

--- src/Message/PxPostAuthorizeRequest.php
+++ src/Message/PxPostAuthorizeRequest.php
@@ -57,6 +57,62 @@ class PxPostAuthorizeRequest extends AbstractRequest
         return $this;
     }
 
+
+    /** 
+     * Get the PxPost EnableAddBillCard
+     *
+     * Optional boolean value to enable the creation of a repeat billing token
+     * when purchasing.
+     *
+     * @return mixed
+     */
+    public function getEnableAddBillCard()
+    {   
+        if (is_null($this->getParameter('enableAddBillCard')))
+          return false;
+
+        return $this->getParameter('enableAddBillCard');
+    }   
+
+    /** 
+     * Set the PxPost EnableAddBillCard
+     *
+     * @param bool $value
+     * @return $this
+     */
+    public function setEnableAddBillCard($value)
+    {   
+        return $this->setParameter('enableAddBillCard', $value);
+    }   
+
+    /** 
+     * Get the PxPost MerchantReference
+     *
+     * Optional string value to allow merchant to attach their own reference
+     * to the transaction.
+     *
+     * @return mixed
+     */
+    public function getMerchantReference()
+    {   
+        if (is_null($this->getParameter('merchantReference')))
+          return false;
+
+        return $this->getParameter('merchantReference');
+    }
+
+    /** 
+     * Set the PxPost MerchantReference
+     *
+     * @param bool $value
+     * @return $this
+     */
+    public function setMerchantReference($value)
+    {
+        return $this->setParameter('merchantReference', $value);
+    }
+
+
     /**
      * Get the PxPost TxnData1
      *
@@ -186,6 +242,13 @@ class PxPostAuthorizeRequest extends AbstractRequest
         if ($this->getReceiptEmail()) {
             $data->ReceiptEmail = $this->getReceiptEmail();
         }
+
+        $data->EnableAddBillCard = ($this->getEnableAddBillCard()) ? 1 : 0;
+
+        if ($this->getMerchantReference()) {
+          $data->MerchantReference = $this->getMerchantReference();
+        }
+
 
         return $data;
     }
